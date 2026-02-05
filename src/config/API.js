ed6a64e20f8b28f29ccc281827ba585423ee7500
samudra1024 <samudrahosmata@gmail.com>
2026-02-05 21:58:02 +0530

    API changed

--- src/config/API.js
+++ src/config/API.js
@@ -1,1 +1,1 @@
-export const BaseURL = 'http://192.168.1.2:8000/api' // Replace with your computer's local IP address 
+export const BaseURL = 'http://192.168.1.10:8000/api' // Replace with your computer's local IP address 
